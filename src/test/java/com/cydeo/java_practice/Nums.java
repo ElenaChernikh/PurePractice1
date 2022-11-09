f3b754645ec9b0194a0148de5f3e030823b43ab6
Cydeo-ElenaCh <chernikhlena@gmail.com>
2022-11-08 21:05:00 -0500

    changed num value2

--- src/test/java/com/cydeo/java_practice/Nums.java
+++ src/test/java/com/cydeo/java_practice/Nums.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Nums {
     public static void main(String[] args) {
 
-String num1= "9";
+String num1= "90";
         System.out.println(num1.length());
         int num2= Integer.parseInt(num1);
         
